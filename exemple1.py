5aa1cfc9fe9aa501a08da6740d79b449cd28625f
khalil <khalillakhdharatc@gmail.com>
2023-02-23 17:40:39 +0100

    Initial commit

---
+++ exemple1.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr = np.array([42,30,22,44])
+print(arr)
+arr2=np.array([[[22,1,3],[2,4,-3]],[[3,2,1],[2,3,4]]])
+print(arr2)
+print("la dimension du tableau est: ",arr2.ndim)
+print("l'élément numéro 3 du tableau 1 est ",arr[3])
+print(arr2[0, 1, 2])
---
+++ numpyintro.py
@@ -0,0 +1,6 @@
+import numpy as np #alias for numpy
+arr = np.array([1, -2,30,6,25 ])
+print(np.__version__)
+print(type(arr))
+
+#print(arr)
